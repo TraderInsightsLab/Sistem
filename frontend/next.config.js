6fca48f1843332371f8c0b459f4056f75f296b8e
Mincu Paul Alexandru <mincuav@gmail.com>
2025-10-19 13:30:25 +0300

    Force Vercel cache bust

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,6 +3,7 @@ const path = require('path');
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  reactStrictMode: true,
   env: {
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
     NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY,
